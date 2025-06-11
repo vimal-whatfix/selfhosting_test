9c857f87be23e0ee0e2aa625a9426a008946115c
vimal kumar <vimal.k@whatfix.com>
2025-06-11 12:37:44 +0530

    test

--- e32f711d-49ed-44e2-8600-ecade64ff12b/whatfix/core/mobile-core.js
+++ e32f711d-49ed-44e2-8600-ecade64ff12b/whatfix/core/mobile-core.js
@@ -4330,6 +4330,7 @@ function fetchConfig(_a, traceId) {
                 case 1:
                     status = _r.sent();
                     config = getSelfHostedConfig();
+ console.log('config===>',config);
                     if (!config) {
                         return [2];
                     }
